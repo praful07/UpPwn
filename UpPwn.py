f548407f70286a414aeb00a4fb1bf64b60cbf235
praful07 <65415097+praful07@users.noreply.github.com>
2020-10-01 11:04:19 +0530

    Update UpPwn.py

--- UpPwn.py
+++ UpPwn.py
@@ -1,7 +1,7 @@
 from lib.opts import Opts
 from lib.core import Core
 import sys
-
+#opts is a simple python library which allows you to easiely parse command line arguments
 def UpPwn(argv):
     opts = Opts(argv)
     opts.initialize_options()
